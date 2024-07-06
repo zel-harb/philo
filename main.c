e68b1575dd193e01a34d9e2522421e44738af363
Zahira El Harba <zel-harb@c3r9p6.1337.ma>
2024-07-06 13:40:46 +0100

    philo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zel-harb <zel-harb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:50:28 by zel-harb          #+#    #+#             */
-/*   Updated: 2024/07/06 10:50:34 by zel-harb         ###   ########.fr       */
+/*   Updated: 2024/07/06 11:46:40 by zel-harb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,55 +18,55 @@ void *print()
     printf("hello\n");
     return NULL;
 }
-void init_threads(t_philo *philo,int ac,char **av)
+void init_threads(t_data *data,int ac,char **av)
 {
     int i;
     i = 0;
+    data->num_philo = ft_atoi(av[1]);
+    data->philo= malloc(sizeof(t_philo)*data->num_philo);
     while(i < ac -1)
     {
-        philo[i].id = i + 1;
+        data->philo[i].id = i + 1;
         // philo[i].fork = i + 1;
-        philo[i].num_philo = ft_atoi(av[1]);
-        philo[i].time_die = ft_atoi(av[2]) ;
-        philo[i].time_eat =ft_atoi(av[3]);
-        philo[i].time_sleep =ft_atoi(av[4]);
-        
+        data->time_die = ft_atoi(av[2]) ;
+        data->time_eat =ft_atoi(av[3]);
+        data->time_sleep =ft_atoi(av[4]);
         i++;
     }
 }
-void get_forks()
-{
+// void get_forks()
+// {
     
-}
+// }
 void *routin(void *arg)
 {
  
     t_philo *philo = (t_philo *)arg;
     pthread_mutex_lock(&mutex);
-    
+    printf("hi-->**%d\n", philo->id);
      pthread_mutex_unlock(&mutex);
     return NULL;
 }
-void creat_threads(t_philo *philo)
+void creat_threads(t_data *data)
 {
     int i;
 
     i = 0;
-    while(i < philo[0].num_philo)
+    while(i < data->num_philo)
     {
-        pthread_create(&philo[i].thread,NULL,routin,&philo[i]);
-        pthread_join(philo[i].thread, NULL) ;
+        pthread_create(&data->philo[i].thread,NULL,routin,&data->philo[i]);
+        pthread_join(data->philo[i].thread, NULL) ;
         i++;
     }
 }
 
 int main(int ac,char **av)
 {
-    t_philo philo[ac -1];
+    t_data data;
     pthread_t thread;
     check_arg(ac,av);
-    init_threads(philo,ac,av);
-    creat_threads(philo);
+    init_threads(&data,ac,av);
+    creat_threads(&data);
     pthread_create(&thread,NULL,print,NULL);
     pthread_join(thread,NULL);
     return 0;
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: zel-harb <zel-harb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 17:43:52 by zel-harb          #+#    #+#             */
-/*   Updated: 2024/07/06 09:40:54 by zel-harb         ###   ########.fr       */
+/*   Updated: 2024/07/06 11:43:19 by zel-harb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,28 @@ typedef struct s_philo
 {
     pthread_t thread;
     int     id;
-    int     time_eat;
-    int     time_die;
-    int     time_sleep;
     int     last_to_eat;
-    int     num_philo;
+   
     pthread_mutex_t *fork;
-       pthread_mutex_t mutex;
+      
     
 } t_philo;
 
+typedef struct s_data
+{
+    int     num_philo;
+    int     time_eat;
+    int     time_die;
+    int     time_sleep;
+    pthread_mutex_t *fork;
+    pthread_mutex_t mutex;
+   t_philo         *philo;
+} t_data;
+
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
-int	ft_strlen(char const *s);
-void check_arg(int ac,char **av);
-int	ft_atoi(const char *str);
+int	    ft_strlen(char const *s);
+void    check_arg(int ac,char **av);
+int	    ft_atoi(const char *str);
 
 #endif
