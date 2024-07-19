d215b0ba7623cf9e336fa435286efe4430e99a9b
Zahira El Harba <zel-harb@c3r8p3.1337.ma>
2024-07-19 13:34:08 +0100

    f_philo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zel-harb <zel-harb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 14:34:49 by zel-harb          #+#    #+#             */
-/*   Updated: 2024/07/17 01:37:31 by zel-harb         ###   ########.fr       */
+/*   Updated: 2024/07/19 00:57:01 by zel-harb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void init_threads(t_data *data,int ac,char **av)
     int i;
     i = 0;
     data->num_philo = ft_atoi(av[1]);
+    data->dead =0;
     data->forks = malloc(sizeof(pthread_mutex_t) *data->num_philo);
     data->philo= malloc(sizeof(t_philo)*data->num_philo);
     while(i < data->num_philo)
@@ -25,6 +26,7 @@ void init_threads(t_data *data,int ac,char **av)
         data->philo[i].time_die = ft_atoi(av[2]);
         data->philo[i].time_eat= ft_atoi(av[3]);
         data->philo[i].time_sleep= ft_atoi(av[4]);
+        data->philo[i].dead = &data->dead;
         data->philo[i].last_time_eat = 0;
         i++;
     }
@@ -46,11 +48,16 @@ void get_forks(t_data *data)
    i = 0;
    while(i < data->num_philo)
    {
-        data->philo[i].r_fork= &data->forks[i];
-         if(i  == data->num_philo - 1)
-            data->philo[i].l_fork = &data->forks[0];
-        else 
+        if(i  == data->num_philo - 1)
+        {
+            data->philo[i].l_fork = &data->forks[i];
+            data->philo[i].r_fork = &data->forks[0];
+        }
+        else
+        {
+            data->philo[i].r_fork= &data->forks[i];
             data->philo[i].l_fork = &data->forks[i +1];
+        }
         i ++;
         
     } 
@@ -60,13 +67,25 @@ void get_forks(t_data *data)
 void creat_threads(t_data *data)
 {
     int i;
+  
     i = 0;
-
     while(i < data->num_philo)
     {
         pthread_create(&data->philo[i].thread,NULL,&routin,&data->philo[i]);
         i++;
     }
+    // while(j < data->num_philo)
+    // {
+       
+    //     if(get_time() - *data->philo[j].dead > data->philo[j].time_die)
+    //         *data->philo[j].dead = 1;
+    //         j++;
+    //      if(j == data->num_philo)
+    //         j =0;
+    //      //  break;
+
+
+    // } 
     i = 0;
     while(i < data->num_philo)
     {
@@ -75,14 +94,19 @@ void creat_threads(t_data *data)
     }
 }
 
+
 int main(int ac,char **av)
 {
     t_data data;
+    pthread_t thread;
     check_arg(ac,av);
     init_threads(&data,ac,av);
     init_forks(&data);
     get_forks(&data);
     creat_threads(&data);
-    
+    // while(0)
+    // {
+    //     printf("hiii\n");
+    // }
     
 }
--- routin.c
+++ routin.c
@@ -6,7 +6,7 @@
 /*   By: zel-harb <zel-harb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 01:18:22 by zel-harb          #+#    #+#             */
-/*   Updated: 2024/07/17 01:39:32 by zel-harb         ###   ########.fr       */
+/*   Updated: 2024/07/19 00:48:05 by zel-harb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,13 @@ void ft_usleep(size_t milliseconds)
 }
 void eating(t_philo *philo)
 {
-    if(philo->id_philo % 2 != 0)
-        ft_usleep(100);
+    
     pthread_mutex_lock(philo->r_fork);
     pthread_mutex_lock(philo->l_fork);
     printf("philosopher %d has taken a fork\n",philo->id_philo);
     printf("philosopher %d is eating\n",philo->id_philo);
     ft_usleep(philo->time_eat);
+    philo->last_time_eat = get_time();
     pthread_mutex_unlock(philo->r_fork);
     pthread_mutex_unlock(philo->l_fork);
 }
@@ -54,8 +54,14 @@ void *routin(void *arg)
 {
     t_philo *philo;
     philo = (t_philo *)arg;
-    eating(philo);
-    sleeping(philo);
-    thinking(philo);
+    if(philo->id_philo % 2 != 0)
+        ft_usleep(100);
+    while(*philo->dead == 0)
+    {  
+        eating(philo);
+        sleeping(philo);
+        thinking(philo);
+    }
+    
     return NULL;
 }
